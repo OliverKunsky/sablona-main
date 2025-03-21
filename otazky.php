2df43f8621195f9e369d91dc16f205c102e33ec9
OliverKunsky <oliver.kunsky@student.ukf.sk>
2025-03-21 10:54:48 +0100

    Initial commit

---
+++ otazky.php
@@ -0,0 +1,15 @@
+<?php
+$otazky = array(
+    "Aký je hlavný rozdiel medzi rastlinnými a živočíšnymi bunkami?",
+    "Kedy sa uskutočnila prvá lunárna expedícia?",
+    "Čo je to fotosyntéza?",
+    "Kto napísal knihu '1984'?",
+    "Aké je hlavné mesto Francúzska?",
+)
+$odpvoede = array(
+    "Rastlinné bunky obsahujú chloroplasty a majú pevnejšiu bunkovú stenu, zatiaľ čo živočíšne bunky tieto vlastnosti nemajú.",
+    "Prvá lunárna expedícia sa uskutočnila 20. júla 1969, keď astronauti Neil Armstrong a Buzz Aldrin pristáli na Mesiaci počas misie Apollo 11.",
+    "Fotosyntéza je proces, pri ktorom rastliny a niektoré mikroorganizmy používajú slnečné svetlo, vodu a oxidu uhličitý na výrobu potravy vo forme glukózy.",
+    "Knihu '1984' napísal George Orwell a bola vydaná v roku 1949.",
+     "Hlavné mesto Francúzska je Paríž.",
+);
